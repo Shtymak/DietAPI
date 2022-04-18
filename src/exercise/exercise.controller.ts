11972fa182e6d6b3de84d3e57ac3f8bd7c582743
Shtymak <vova.shtimak@gmail.com>
2022-04-18 23:21:56 +0300

    [NEW] Create exercise

--- src/exercise/exercise.controller.ts
+++ src/exercise/exercise.controller.ts
@@ -1,9 +1,15 @@
 import { Controller } from '@nestjs/common';
 import { ExerciseService } from './exercise.service';
 import { ApiTags } from "@nestjs/swagger";
+import { CreateExerciseDto } from './dto/create-exercise.dto';
 
 @Controller('exercise')
 @ApiTags('Вправи')
 export class ExerciseController {
   constructor(private readonly exerciseService: ExerciseService) {}
+
+  async create(exercise: CreateExerciseDto) {
+    const createdExercise = await this.exerciseService.create(exercise);
+    return createdExercise;
+  }
 }
